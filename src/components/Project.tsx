11231590bfa60d6a84067a1c5bdc53fdb04c46a8
Elijah5399 <chia.elijah@gmail.com>
2023-07-21 03:51:38 +0800

    Added octokit and projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,32 +1,61 @@
+"use client";
+import { Octokit } from "@octokit/core";
 import { BsArrowUpRight } from "react-icons/bs";
+
+//Use octokit to increase the API rate limit
+const octokit = new Octokit({ auth: process.env.NEXT_PUBLIC_AUTH_KEY });
+
+function dateConverter(init: string): string {
+  const date = new Date(init);
+  const nowdate = new Date();
+  const diff = nowdate.getTime() - date.getTime();
+  const hours = Math.trunc(diff / 1000 / 60 / 60);
+
+  if (hours < 24) {
+    if (hours < 1) return "just now";
+    let measurement = hours === 1 ? "hour" : "hours";
+    return `${hours.toString()} ${measurement} ago`;
+  } else {
+    const options = { day: "numeric", month: "long", year: "numeric" } as const;
+    const time = new Intl.DateTimeFormat("en-US", options).format(date);
+    return `on ${time}`;
+  }
+}
 
 export default function Project({
   title,
-  imageLink,
   description,
   projectLink,
-  skills,
+  langLink,
+  date,
 }: {
   title: string;
-  imageLink: string;
   description: string;
   projectLink: string;
-  skills: string[];
+  langLink: string;
+  date: string;
 }) {
   return (
     <a href={projectLink} className="group">
-      <div className="dark:group-hover:bg-stone-900/75 group-hover:bg-gray-100/75 rounded-md pl-36 py-4">
-        <span className="group-hover:text-emerald-300 text-2xl font-semibold">
-          {title}{" "}
-          <span>
-            <BsArrowUpRight
-              size={15}
-              className="group-hover:text-emerald-400 dark:text-white text-black dark:group-hover:text-emerald-400 inline-block ml-1 mb-1
+      <div className="dark:group-hover:bg-stone-900/75 group-hover:bg-emerald-100/75 rounded-md py-4 grid grid-cols-6">
+        <div className="col-start-1 col-end-2 pr-4 pt-1">
+          <p className="text-sm">Last updated: {dateConverter(date)}</p>
+        </div>
+        <div className="col-span-5 col-end-7">
+          <span className="group-hover:text-emerald-300 text-2xl font-semibold">
+            {title}{" "}
+            <span>
+              <BsArrowUpRight
+                size={15}
+                className="group-hover:text-emerald-400 dark:text-white text-black dark:group-hover:text-emerald-400 inline-block ml-1 mb-1
               group-hover:translate-x-1 group-hover:-translate-y-1"
-            />
+              />
+            </span>
           </span>
-        </span>
-        <p>{description}</p>
+          <p>
+            {description ? description : "No description for this project yet."}
+          </p>
+        </div>
       </div>
     </a>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,13 +1,60 @@
+"use client";
 import Project from "./Project";
+import { useState, useCallback, useEffect } from "react";
+import { Octokit } from "@octokit/core";
 
-export default function Projects() {
+//Use octokit to increase the API rate limit
+const octokit = new Octokit({ auth: process.env.NEXT_PUBLIC_AUTH_KEY });
+const allReposURL = "/user/repos?sort=updated&direction=desc";
+const specificRepoURL = `/repos/${process.env.NEXT_PUBLIC_GITHUBUSERNAME}`;
+
+export default function Projects({ numProjects }: { numProjects: number }) {
+  //numProjects is the number of projects we retrieve from GitHub
+  const [projectsArray, setProjectsArray] = useState<any[]>([]);
+  const fetchRepos = useCallback(async () => {
+    let repoList = [];
+    try {
+      // getting all repos
+      const response = await octokit.request(allReposURL);
+      // slicing to the length
+      repoList = [...response.data.slice(0, numProjects)]; //set to 4 projects retrieved
+      // adding specified repos
+      let updList = [];
+      try {
+        for (let repoName of repoList) {
+          const response = await octokit.request(
+            `${specificRepoURL}/${repoName.name}`
+          );
+          updList.push(response.data);
+          console.log("pushing data: " + JSON.stringify(response.data));
+        }
+      } catch (error: any) {
+        console.error(error.message);
+      }
+      console.log("repoList length is: " + repoList.length);
+      // setting projectArray
+      setProjectsArray(updList);
+    } catch (error: any) {
+      console.error(error.message);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchRepos();
+  }, []);
+
   return (
-    <Project
-      title="title1"
-      imageLink="blabla"
-      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, molestias, quae illum ipsam vel architecto ut, error esse beatae quibusdam illo! Rerum ipsum ipsam dolores nemo quam eum esse doloremque."
-      projectLink="blabla"
-      skills={["React", "TypeScript"]}
-    />
+    <>
+      {projectsArray.map((proj) => (
+        <Project
+          key={proj.name}
+          title={proj.name}
+          description={proj.description}
+          projectLink={proj.svn_url}
+          langLink={proj.languages_url}
+          date={proj.pushed_at}
+        />
+      ))}
+    </>
   );
 }
