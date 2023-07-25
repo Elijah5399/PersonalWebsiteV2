08ae8ab4a8bfdb32b63cfc2b2d8224993113ed70
Elijah5399 <chia.elijah@gmail.com>
2023-07-25 18:27:45 +0800

    Removed rewrites function:

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://leetcode.com/graphql/:path*",
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
